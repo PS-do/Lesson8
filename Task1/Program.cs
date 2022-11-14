425ca7aa11869c8185a65c89401b5fee0cd64471
Сергей Плюхин <psg.pgs@mail.ru>
2022-11-14 20:11:45 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\LessonTemp";
+            //string[] files = Directory.GetFiles(path, "*.txt", SearchOption.AllDirectories);
+            string[] directories = Directory.GetDirectories(path);
+            string[] files = Directory.GetFiles(path);
+            foreach (string directory in directories)
+            {
+                Console.WriteLine(files);
+                files = Directory.GetFiles(path);
+                foreach (string file in files)
+                    Console.WriteLine(directory);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2.    Обязательная задача*. \n" +
+                "Программно создайте текстовый файл и запишите в него 10 случайных чисел. \n" +
+                "Затем программно откройте созданный файл, рассчитайте сумму чисел в нем, ответ выведите на консоль.");
+
+            string patchFolder = "Temp_txt";
+            string nameFile = "numbers.txt";
+            string path_f = Path.Combine(patchFolder, nameFile);
+
+            Random random = new Random();
+            if (!Directory.Exists(patchFolder))
+            {
+                Directory.CreateDirectory(patchFolder);
+            }
+
+            using (StreamWriter sw = new StreamWriter(path_f, false))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(random.Next(-10, 10));
+                }
+            }
+            int sum = 0;
+            using (StreamReader sr = new StreamReader(path_f)) 
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sum += Convert.ToInt32(sr.ReadLine());
+                }
+            }
+            Console.WriteLine("Sum={0}",sum);
+            Console.ReadKey();
+
+        }
+    }
+}
